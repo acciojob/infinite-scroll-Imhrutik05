1cefd57862f1ba07e8133bd0454459c75a01a5d7
Hrutik Chaudhari <hrutikchaudhari05@gmail.com>
2025-09-22 00:54:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-//your code here!
+function addItems(items) {
+	let ol = document.getElementById('infi-list');
+	for (let i = 0; i < items; i++){
+		let li = document.createElement('li');
+		li.textContent = `Item ${ol.children.length + 1}`;
+		ol.appendChild(li);
+	}
+	setTimeout(checkScroll, 50);  // Delay check to allow DOM update
+}
 
+function checkScroll() {
+	let ol = document.getElementById('infi-list');
+	if (ol.scrollTop + ol.clientHeight >= ol.scrollHeight - 1) {
+		addItems(2);
+	}
+}
+
+document.getElementById('infi-list').addEventListener('scroll', checkScroll);
+addItems(10);
